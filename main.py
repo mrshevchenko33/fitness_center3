773c4e8fb36785025bbac3b067f1749f9059fb20
mrshe <mrshevchenko33@gmail.com>
2024-05-14 21:44:25 +0300

    commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/register', methods=['GET', 'POST'])
 def get_register():
     if request.method == 'POST':
-        return 'new ser register'
+        return 'new user registered'
     else:
-        return 'please sign to registartion'
+        return 'please sign in to registration'
 
 @app.route('/login', methods=['GET', 'POST'])
 def get_login():
@@ -21,39 +21,39 @@ def get_user():
     if request.method == 'POST':
         return 'user data modified'
     elif request.method == 'PUT':
-        return 'user info is succes updated'
+        return 'user info successfully updated'
     else:
         return 'user info'
 
-@app.route('/funds', methods= ['GET', 'POST'])
+@app.route('/funds', methods=['GET', 'POST'])
 def get_funds():
     if request.method == 'POST':
-        return 'user account was succecfully funded'
+        return 'user account was successfully funded'
     else:
         return 'user deposit value'
 
 @app.route('/fitness_center/<gym_id>/services/', methods=['GET'])
 def get_services(gym_id):
-    return  f'fitness center {gym_id} services list'
+    return f'fitness center {gym_id} services list'
 
 @app.route('/fitness_center/<gym_id>/services/<service_id>', methods=['GET'])
-def service_id(gym_id, service_id):
-    return  f'fitness center {gym_id} service {service_id} info'
+def service_info(gym_id, service_id):
+    return f'fitness center {gym_id} service {service_id} info'
 
 @app.route('/fitness_center/<gym_id>/trainer', methods=['GET'])
 def get_trainer(gym_id):
-    return  f'fitness center {gym_id} trainers list'
+    return f'fitness center {gym_id} trainers list'
 
-@app.route('/fitness_center/<gym_id>/services/<trainer_id>', methods=['GET'])
-def trainer_id(gym_id, trainer_id):
-    return  f'fitness center {gym_id} trainer {trainer_id} info'
+@app.route('/fitness_center/<gym_id>/trainer/<trainer_id>', methods=['GET'])
+def trainer_info(gym_id, trainer_id):
+    return f'fitness center {gym_id} trainer {trainer_id} info'
 
 @app.route('/fitness_center/<gym_id>/trainer/<trainer_id>/rating', methods=['GET','POST', 'PUT'])
 def trainer_rating(gym_id, trainer_id):
     if request.method == 'POST':
-        return  f'rewiew of {trainer_id} in {gym_id} has been added'
+        return f'review of {trainer_id} in {gym_id} has been added'
     elif request.method == 'PUT':
-        return  f'rewiew of {trainer_id} in {gym_id} has been edited'
+        return f'review of {trainer_id} in {gym_id} has been edited'
     else:
         return f'rating of {trainer_id} in {gym_id}'
 
@@ -82,17 +82,17 @@ def user_checkout():
     elif request.method == 'PUT':
         return 'update user checkout'
 
-@app.route('/fitness_center', methods=['GET'])
+@app.route('/fitness_centers', methods=['GET'])
 def fitness_centers():
     return 'get all fitness centers'
 
-@app.route('/fitness_center/<id>', methods=['GET'])
-def fitness_center(id):
-    return f'get fitness center with id {id}'
+@app.route('/fitness_center/<center_id>', methods=['GET'])
+def fitness_center(center_id):
+    return f'get fitness center with id {center_id}'
 
-@app.route('/fitness_center/<id>/loyalty_programs', methods=['GET'])
-def loyalty_programs(id):
-    return f'get loyalty programs for fitness center with id {id}'
+@app.route('/fitness_center/<center_id>/loyalty_programs', methods=['GET'])
+def loyalty_programs(center_id):
+    return f'get loyalty programs for fitness center with id {center_id}'
 
 if __name__ == '__main__':
     app.run(debug=True)
